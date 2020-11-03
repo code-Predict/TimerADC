d4a3576766180f31914185226e61dbe895d83c0a
Enchan <enchant0130@gmail.com>
2020-11-04 08:20:22 +0900

    [Update] interrupt vector

--- TimerADC.ino
+++ TimerADC.ino
@@ -7,22 +7,41 @@
 #define VREF 2.048
 #define VFSR VREF/PGA
 #define FSR (((long int)1<<23)-1)
+#define INTVECT_SIZE 5
 
 #define ADS1220_CS_PIN    16
 #define ADS1220_DRDY_PIN  4
 
 //
-volatile unsigned int interruptCounter;
 portMUX_TYPE timerMux = portMUX_INITIALIZER_UNLOCKED;
+volatile unsigned int interruptCounter[INTVECT_SIZE] = {};
+void (*intVect[])() = {
+    dumpADCValue,
+    nopVect,
+    nopVect,
+    nopVect,
+    nopVect
+};
+
+// 割り込みベクタのパディング
+void nopVect(){}
 
 //
 ADCAccessor adc(ADS1220_CS_PIN, ADS1220_DRDY_PIN);
 
-// ISR
+// ISR: AD変換終了
 void IRAM_ATTR onDataReady() {
     portENTER_CRITICAL_ISR(&timerMux);
-    interruptCounter++;
+    interruptCounter[0]++;
     portEXIT_CRITICAL_ISR(&timerMux);
+}
+
+// 変換終了時の処理
+void dumpADCValue(){
+    // ADCから値を取得し、表示
+    int32_t adcValue = adc.readADCValue();
+    float Vout = (float)((adcValue * VFSR * 1000) / FSR);
+    Serial.println(Vout);
 }
 
 void setup(){
@@ -38,16 +57,15 @@ void setup(){
 }
 
 void loop(){
-
-    if (interruptCounter > 0) {
-        // 割込みカウンタを減らす
-        portENTER_CRITICAL(&timerMux);
-        interruptCounter--;
-        portEXIT_CRITICAL(&timerMux);
-
-        // ADCから値を取得し、表示
-        int32_t adcValue = adc.readADCValue();
-        float Vout = (float)((adcValue * VFSR * 1000) / FSR);
-        Serial.println(Vout);
+    // 割込みが発生しているか走査する
+    for(int i = 0; i < INTVECT_SIZE; i++){
+        if(interruptCounter[i] > 0){
+            portENTER_CRITICAL(&timerMux);
+            interruptCounter[i]--;
+            // 割り込みベクタを呼び出す
+            (*intVect[i])();
+            portEXIT_CRITICAL(&timerMux);
+        }
     }
 }
+
